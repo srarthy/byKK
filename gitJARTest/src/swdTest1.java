10e7f41ab9ddd761e1112664b8d5064e5b162039
Arthy <Arthy@DESKTOP-L4HUAOJ>
2020-08-26 10:21:41 -0700

    modified the code

--- gitJARTest/src/swdTest1.java
+++ gitJARTest/src/swdTest1.java
@@ -3,6 +3,9 @@ public class swdTest1 {
 
 	public static void main(String[] args) {
 
+		for (int i = 1 ; i <= 20 ; i++) {
+			System.out.println("This works " + i);
+		}
 		System.out.println("End Program");
 
 	}
