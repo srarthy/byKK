ef2c75b53e244fca397687dc820b49e25868f5e0
Arthy <Arthy@DESKTOP-L4HUAOJ>
2020-08-26 10:25:53 -0700

    30 prints

--- gitJARTest/src/swdTest1.java
+++ gitJARTest/src/swdTest1.java
@@ -3,7 +3,7 @@ public class swdTest1 {
 
 	public static void main(String[] args) {
 
-		for (int i = 1 ; i <= 20 ; i++) {
+		for (int i = 1 ; i <= 30 ; i++) {
 			System.out.println("This works " + i);
 		}
 		System.out.println("End Program");
